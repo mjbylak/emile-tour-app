6e5ff3a956970cef519ac1e0b76ed4c1706448ed
Mitchell B. <mitchell.bylak@gmail.com>
2024-04-24 00:46:40 +0200

    update/adding-chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
+    <ChakraProvider>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +25,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ChakraProvider>
   );
 }
 
